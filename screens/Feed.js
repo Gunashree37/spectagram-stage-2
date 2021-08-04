d7d3011edfa5a05c6229ee74812dfe40a35513c5
Gunashree S <gunashrees341@gmail.com>
2021-08-04 18:17:20 +0530

    Update Feed.js

--- screens/Feed.js
+++ screens/Feed.js
@@ -33,16 +33,15 @@ export default class Feed extends Component {
         return (
             <View style={styles.container}>
                 <SafeAreaView style={styles.droidSafeArea} />
-                <View style={styles.appTitle}>
-                    <View style={styles.appIcon}>
-                        <Image
-                            source={require("../assets/logo.png")}
-                            style={styles.iconImage}
-                        ></Image>
-                    </View>
-                    <View style={styles.appTitleTextContainer}>
-                        <Text style={styles.appTitleText}>Spectagram</Text>
-                    </View>
+                
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
                 </View>
                 <View style={styles.cardContainer}>
                     <FlatList
